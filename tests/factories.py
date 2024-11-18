e18ea37d8c9a88154b49febb1db0027d7005f005
Michaeluwakz <90646867+Michaeluwakz@users.noreply.github.com>
2024-11-18 13:00:11 +0100

    Update factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -19,16 +19,23 @@ Test Factory to make fake objects for testing
 """
 import factory
 from factory.fuzzy import FuzzyChoice, FuzzyDecimal
-from service.models import Product, Category
-
+from service.models import Product
 
 class ProductFactory(factory.Factory):
-    """Creates fake products for testing"""
+    """Creates fake products for testing."""
 
     class Meta:
-        """Maps factory to data model"""
-
         model = Product
 
-    id = factory.Sequence(lambda n: n)
-   ## Add code to create Fake Products 
+    name = FuzzyChoice(
+        choices=[
+            "Hat", "Pants", "Shirt", "Apple", "Banana",
+            "Pots", "Towels", "Ford", "Chevy", "Hammer", "Wrench"
+        ]
+    )
+    description = factory.Faker("text")
+    price = FuzzyDecimal(0.5, 2000.0, precision=2)
+    available = FuzzyChoice(choices=[True, False])
+    category = FuzzyChoice(
+        choices=["UNKNOWN", "CLOTHS", "FOOD", "HOUSEWARES", "AUTOMOTIVE", "TOOLS"]
+    )
